660f1f6c4b8ec7fbb35d93851b8ed385c3c161f9
Aung Thu Hein <agthuhein.mm@gmail.com>
2025-09-14 21:54:32 +0200

    Nav and landing apge

--- DEVCONNECTOR/client/src/App.js
+++ DEVCONNECTOR/client/src/App.js
@@ -1,13 +1,38 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        {/* Public routes */}
+        <Route path='/' element={<Landing />} />
+        <Route
+          path='/register'
+          element={
+            <section className='container'>
+              <Register />
+            </section>
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <section className='container'>
+              <Login />
+            </section>
+          }
+        />
+      </Routes>
+    </Fragment>
+  </Router>
 );
 
 export default App;
--- DEVCONNECTOR/client/src/components/layout/Landing.js
+++ DEVCONNECTOR/client/src/components/layout/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Landing = () => {
+const Landing = () => {
   return (
     <section className='landing'>
       <div class='dark-overlay'>
