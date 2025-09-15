e1249831c0ab3c27e1a24de50986c3a0487f40ca
Aung Thu Hein <agthuhein.mm@gmail.com>
2025-09-15 15:03:30 +0200

    logout

--- DEVCONNECTOR/client/src/actions/auth.js
+++ DEVCONNECTOR/client/src/actions/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 import { USER_LOADED, AUTH_ERROR } from './types';
-import { LOGIN_SUCCESS, LOGIN_FAIL } from './types';
+import { LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from './types';
 
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -77,4 +77,9 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout / Clear Profile
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT });
 };
